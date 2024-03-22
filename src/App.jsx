68661f3c9c64bbc1bcef9760e4549f38778cb08d
Karlito14 <leiroz26@hotmail.com>
2024-03-22 16:52:19 +0100

    header et responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Navigation } from './components/Navigation/Navigation';
+
 export const App = () => {
     return (
-        <div className="text-gray-500 text-3xl">
-            Hello tailwind
+        <div className='p-10 xl:px-24'>
+            <Navigation />
         </div>
     )
 };
