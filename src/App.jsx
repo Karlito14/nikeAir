856fc24cb1e8b413a28780df4fc67fca80910001
Karlito14 <leiroz26@hotmail.com>
2024-03-23 11:28:43 +0100

    détails chaussures

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { Navigation } from './components/Navigation/Navigation';
+import { ShoesDetails } from './components/ShoesDetails/ShoesDetails';
+import shoesImg from './assets/n1-min.png';
+import { useState } from 'react';
 
 export const App = () => {
+    const [image, setImage] = useState(shoesImg)
     return (
         <div className='p-10 xl:px-24'>
             <Navigation />
+            <ShoesDetails img={image} />
         </div>
     )
 };
