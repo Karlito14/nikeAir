bbbcc507e076679cea1a5a7d723f749970aae02c
Karlito14 <leiroz26@hotmail.com>
2024-03-25 17:50:03 +0100

    animation sidebar

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -4,7 +4,7 @@ import { TbShoppingBag } from 'react-icons/tb';
 import { NavList } from '../NavList/NavList';
 import { useState } from 'react';
 
-export const Navigation = () => {
+export const Navigation = ({ openSidebar }) => {
     const [displayNav, setDisplayNav] = useState(true);
     return (
         <header className='flex flex-wrap justify-between items-center'>
@@ -17,7 +17,10 @@ export const Navigation = () => {
             <nav className={`${!displayNav ? 'hidden' : '' }hidden w-full lg:w-auto lg:block`}>
                 <NavList />
             </nav>
-            <div className='button-bag lg:mr-8 btn-press-animation z-10'>
+            <div className='button-bag lg:mr-8 btn-press-animation z-10' onClick={(event) => {
+                event.stopPropagation();
+                openSidebar(true);
+            }}>
                 <TbShoppingBag />
             </div>
         </header>
